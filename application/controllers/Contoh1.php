66e148dfcf7570a87d303b658afff68f562a6d53
fihri <90844910+fihri@users.noreply.github.com>
2021-09-28 22:02:47 +0700

    Add files via upload

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ahmad Fihri saya tinggal di daerah jakarta pusat olahraga yang saya sukai adalah sepak bola";
+    }
+}
