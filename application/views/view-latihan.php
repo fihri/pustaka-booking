86e8f6e5ebfe290e6f3cec7d93824c4e1ae14166
fihri <90844910+fihri@users.noreply.github.com>
2021-11-09 20:19:05 +0700

    Add files via upload

---
+++ application/views/view-latihan.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo kawan.. Yuk kita belajar web programing..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari permodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
